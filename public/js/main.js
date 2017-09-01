82060bcad42ac488d7388569b6bcfdd544346f9e
roudhentel <roudhentel.i.nagoy@gmail.com>
2017-09-01 14:44:50 +0800

    completing other css

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('.move-page2').click(function (e) {
         if ($('.move-page2').hasClass('pageonenotvisible')) {
-            var fheight = 40; // first footer has a 40px margin
+            var fheight = $('.footer.f1').outerHeight(true) - $('.footer.f1').innerHeight(); // first footer margin top
             $('.footer').each(function () { var $this = $(this); fheight += $this.height(); });
 
             $('.body-1').show();
@@ -31,7 +31,7 @@ $(document).ready(function () {
 
     $('.move-page3').click(function (e) {
         if ($('.move-page3').hasClass('pageonenotvisible')) {
-            var fheight = $('.page-2 .footer-2.f2').height() + ($('.page-2 .footer-2.f2').height() / 2) ;
+            var fheight = $('.page-2 .footer-2.f2').height() + $('.page-2 .footer-2.f1').height();
 
             $('.footer-2.f1').hide();
             $('.footer').show();
@@ -47,8 +47,8 @@ $(document).ready(function () {
         } else {
             $('.page-3').show();
             setTimeout(function () {
-                var fheight = $('.page-2 .footer-2.f2').height() + ($('.page-2 .footer-2.f2').height() / 2) ;
-                
+                var fheight = $('.page-2 .footer-2.f2').height() + $('.page-2 .footer-2.f1').height();
+
                 $('.body').animate({
                     scrollTop: ($('.page-1').height() + $('.page-2').height()) - fheight
                 }, 800, function () {
@@ -56,8 +56,42 @@ $(document).ready(function () {
                     $('.footer').hide();
                     $('.body-2').hide();
                     $('.body').scrollTop(0);
-                    
+
                     $('.move-page3').addClass('pageonenotvisible');
+                });
+            });
+        }
+    });
+
+    $('.move-page4').click(function (e) {
+        if ($('.move-page4').hasClass('pageonenotvisible')) {
+            var fheight = 0;
+            $('.footer-3').each(function () { var $this = $(this); fheight += $this.height(); });
+
+            $('.page-2').show();
+            $('.body-3').show();
+            $('.move-page4').removeClass('pageonenotvisible');
+            $('.body').scrollTop(($('.page-2').height() + $('.page-3').height()) - fheight);
+            $('.body').animate({
+                scrollTop: 0
+            }, 800, function () {
+                $('.page-4').hide();
+            });
+
+        } else {
+            $('.page-4').show();
+            setTimeout(function () {
+                var fheight = 0;
+                $('.footer-3').each(function () { var $this = $(this); fheight += $this.height(); });
+
+                $('.body').animate({
+                    scrollTop: ($('.page-2').height() + $('.page-3').height()) - fheight
+                }, 800, function () {
+                    $('.page-2').hide();
+                    $('.body-3').hide();
+                    $('.body').scrollTop(0);
+
+                    $('.move-page4').addClass('pageonenotvisible');
                 });
             });
         }
